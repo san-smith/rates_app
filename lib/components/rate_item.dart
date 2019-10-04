7ed4e9c81a271c1190abbaf59f0a6faca61ad096
san-smith <san-smith@mail.ru>
2019-10-05 01:06:39 +0700

    Fix RateItem

--- lib/components/rate_item.dart
+++ lib/components/rate_item.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:rates_app/models/rate.dart';
+import 'package:intl/intl.dart';
+
+var formatter = DateFormat('dd.MM.yyyy');
 
 class RateItem extends StatelessWidget {
   final Rate item;
@@ -10,26 +13,111 @@ class RateItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRect(
-      child: new BackdropFilter(
-        filter: new ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-        child: new Container(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Container(
           height: 80,
           margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-          decoration: new BoxDecoration(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
             color: Color(0xaa272e47).withOpacity(0.5),
-            borderRadius: BorderRadius.all(Radius.circular(2.5)),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
           ),
-          child: new Center(
-            child: new Text(
-              'Entry ${item.code}',
-              style: TextStyle(
-                color: Color(0xfff8ffff),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          child: Row(
+            children: <Widget>[
+              left(),
+              center(),
+              right(),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  left() {
+    return Container(
+      width: 100,
+      height: 80,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            item.code,
+            style: TextStyle(
+              color: Color(0xffcccccc),
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            item.name,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: Color(0xff999999),
+              fontSize: 10,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  center() {
+    return Container(
+      child: Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('${item.value.toStringAsFixed(2)}',
+            style: TextStyle(
+              color: Color(0xffffffff),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
+
+  right() {
+    var diff = item.value - item.previous;
+    var percent = (item.value / item.previous - 1) * 100;
+    return Container(
+      height: 40,
+      width: 80,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Color(diff >= 0 ? 0xff23cc9c : 0xfff26560),
+      ),
+      margin: EdgeInsets.only(left: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '${diff.toStringAsFixed(4)}',
+            style: TextStyle(
+              color: Color(0xffffffff),
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '${percent.toStringAsFixed(2)} %',
+            style: TextStyle(
+              color: Color(0xffeeeeee),
+              fontSize: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
